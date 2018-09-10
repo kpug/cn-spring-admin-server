489cb816d6902d8f2e31ab38153955af53b71012
Joseph Yang <before30@gmail.com>
2018-09-11 01:04:52 +0900

    Add init

---
+++ src/main/java/cc/kpug/cloudnative/springbootadminserver/SpringBootAdminServerApplication.java
@@ -0,0 +1,12 @@
+package cc.kpug.cloudnative.springbootadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
+}
